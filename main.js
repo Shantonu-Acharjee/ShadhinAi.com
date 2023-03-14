29a7519a4a1b3ab32eac3b7ed518e246f12b42d4
Shantonu-Acharjee <shantonuacharjee@gmail.com>
2023-03-14 14:01:11 +0600

    home slider added

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
 /*================================== search-form Start ======================*/
 
 let searchForm = document.querySelector('.search-form');
-document.querySelector('#search-btn').onclick = () =>{
+document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
 }
 
 /*================================== search-form End ======================*/
+
+
+
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    
+    loop:true,
+});
+
+
+
+/*================================== home-slider End ======================*/
