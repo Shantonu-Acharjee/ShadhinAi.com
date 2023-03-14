083b8576ecc0539f66da538f7e9c1b339e9b18c2
Shantonu-Acharjee <shantonuacharjee@gmail.com>
2023-03-14 11:18:27 +0600

    home slider note done

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+/*================================== search-form Start ======================*/
+
 let searchForm = document.querySelector('.search-form');
 document.querySelector('#search-btn').onclick = () =>{
     searchForm.classList.toggle('active');
 }
+
+/*================================== search-form End ======================*/
