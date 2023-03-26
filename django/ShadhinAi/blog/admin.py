21d2df1f74e364af789d7c1700a8b9e8e7a1b99f
Shantonu-Acharjee <shantonuacharjee@gmail.com>
2023-03-26 11:14:12 +0600

    model done for slider

--- django/ShadhinAi/blog/admin.py
+++ django/ShadhinAi/blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Slider
+
+
+
+# for configuration fo Slider admin
+class SliderAdmin(admin.ModelAdmin):
+    list_display = ('id','image_tag','title', 'destination_link', 'post_date')
+    search_fields = ('title', 'alt_data')
+
+
 
 # Register your models here.
+admin.site.register(Slider, SliderAdmin)
--- django/ShadhinAi/blog/models.py
+++ django/ShadhinAi/blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils.html import format_html
+
 
 # Create your models here.
+
+# slider model
+class Slider(models.Model):
+    id = models.AutoField(primary_key= True)
+    image = models.ImageField(upload_to= 'slider/')
+    title = models.CharField(max_length= 100)
+    destination_link = models.TextField(default= '#')
+    alt_data = models.CharField(max_length= 200)
+    post_date = models.DateTimeField(auto_now_add= True, null= True)
+
+
+    def __str__(self):
+        return self.title
+
+    def image_tag(self):
+        return format_html('<img src = "/media/{}"  style = "width: 40px; max-height: 40px;"  />'.format(self.image))
