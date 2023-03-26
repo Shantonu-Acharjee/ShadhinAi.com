6055272fa53d3b9250055ebbfbbbddaae91acb8e
Shantonu-Acharjee <shantonuacharjee@gmail.com>
2023-03-26 08:58:07 +0600

    project page done

--- django/ShadhinAi/blog/urls.py
+++ django/ShadhinAi/blog/urls.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from .views import home
+from .views import home, projects
 
 
 urlpatterns = [
    
     path("", home , name= 'home'),
+    path("projects/", projects , name= 'projects'),
     
 ]
--- django/ShadhinAi/blog/views.py
+++ django/ShadhinAi/blog/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+
+
+
+def projects(request):
+    return render(request, 'projects.html')
