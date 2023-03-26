3fa6f411874612c03e754fc226fb0bf9c3ea14d1
Shantonu-Acharjee <shantonuacharjee@gmail.com>
2023-03-26 13:33:51 +0600

    working for signup section

--- django/ShadhinAi/blog/views.py
+++ django/ShadhinAi/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from blog.models import Slider
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 def home(request):
@@ -34,6 +36,20 @@ def login(request):
 
 
 def signup(request):
+
+    if request.method == 'POST':
+
+        user_name = request.POST.get('user_name')
+        user_email = request.POST.get('user_email')
+        user_password1 = request.POST.get("user_password1")
+        user_password2 = request.POST.get("user_password2")
+
+
+        #my_user = User.objects.create_user(user_name, user_email, user_password1)
+        #my_user.save()
+
+        print(user_name, user_email, user_password1, user_password2)
+
     return render(request, 'signup.html')
 
 
