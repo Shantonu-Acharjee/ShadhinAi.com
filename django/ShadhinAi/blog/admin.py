21a061aa39b187a39f1851f4131f6f1aa7c9c500
Shantonu-Acharjee <shantonuacharjee@gmail.com>
2023-03-26 18:06:18 +0600

    model create for post

--- django/ShadhinAi/blog/admin.py
+++ django/ShadhinAi/blog/admin.py
@@ -1,14 +1,24 @@
 from django.contrib import admin
-from .models import Slider
+from .models import Slider, Post
 
 
 
-# for configuration fo Slider admin
+# for configuration of Slider admin
 class SliderAdmin(admin.ModelAdmin):
     list_display = ('id','image_tag','title', 'destination_link', 'post_date')
     search_fields = ('title', 'alt_data')
 
 
+# for configuration of post admin
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('post_id','image_tag','post_title', 'post_url', 'post_date')
+    search_fields = ('post_title', 'post_description', 'post_short_description')
+
+
+
+
+
 
 # Register your models here.
 admin.site.register(Slider, SliderAdmin)
+admin.site.register(Post, PostAdmin)
--- django/ShadhinAi/blog/models.py
+++ django/ShadhinAi/blog/models.py
@@ -19,3 +19,33 @@ class Slider(models.Model):
 
     def image_tag(self):
         return format_html('<img src = "/media/{}"  style = "width: 40px; max-height: 40px;"  />'.format(self.image))
+
+
+
+
+
+
+
+# post model
+class Post(models.Model):
+
+    post_id = models.AutoField(primary_key= True)
+    post_thumbnail = models.ImageField(upload_to= 'posts/')
+    post_title = models.CharField(max_length= 200)
+    post_url = models.CharField(max_length= 200, unique= True)
+    post_short_description = models.CharField(max_length= 200)
+    post_description = models.TextField()
+    post_date = models.DateTimeField(auto_now_add= True)
+    post_keyword = models.TextField()
+    post_Schema = models.TextField()
+
+
+
+    def __str__(self):
+        return self.post_title
+
+    def image_tag(self):
+        return format_html('<img src = "/media/{}"  style = "width: 40px; max-height: 40px;"  />'.format(self.post_thumbnail))
+
+
+    
