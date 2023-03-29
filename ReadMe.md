ff192e756c11cc423c0fe0c3484a6d72772d99c9
Shantonu-Acharjee <shantonuacharjee@gmail.com>
2023-03-29 22:30:47 +0600

    static file not found

---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+# ShadhinAi-0.1
+
+# Basic Commands
+- `django-admin startproject ShadhinAi .`
+- `python manage.py startapp blog`
---
+++ blog/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+# home
+def home(request):
+    return render(request, 'home.html')
