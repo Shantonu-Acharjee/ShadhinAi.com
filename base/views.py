149a8f1910d2567e3eee882eb85595aa6d93cfa4
Shantonu-Acharjee <shantonuacharjee@gmail.com>
2023-03-31 13:40:45 +0600

    pagenation add done

--- base/views.py
+++ base/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
 from post.models import Blog
+from django.core.paginator import PageNotAnInteger, EmptyPage,Paginator
 
 
 def home(request):
-    blogs = Blog.objects.order_by('-created_date')
+    queryset = Blog.objects.order_by('-created_date')
+    page = request.GET.get('page', 1)
+    paginator = Paginator(queryset, 2) #enter how many post you want to see on single page
+
+    try:
+        blogs = paginator.page(page)
+
+    except EmptyPage:
+        blogs = paginator.page(1)
+
+    except PageNotAnInteger:
+            blogs = paginator.page(1)
+
+
     context = {
-        'blogs' : blogs
+        'blogs' : blogs,
+        'paginator' : paginator,
     }
+
     return render(request, 'home/home.html', context)
