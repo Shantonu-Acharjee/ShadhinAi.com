319eb19af4c598b984209221258be51049606d3d
Shantonu-Acharjee <shantonuacharjee@gmail.com>
2023-03-31 14:03:50 +0600

    pagination add on blogs page

--- post/views.py
+++ post/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
-from .models import Blog, Tag
+from .models import Blog
+from django.core.paginator import PageNotAnInteger, EmptyPage,Paginator
+
 
 def blogs(request):
-    blogs = Blog.objects.order_by('-created_date')
+
+    queryset = Blog.objects.order_by('-created_date')
+    page = request.GET.get('page', 1)
+    paginator = Paginator(queryset, 1) #enter how many post you want to see on single page
+
+    try:
+        blogs = paginator.page(page)
+
+    except EmptyPage:
+        blogs = paginator.page(1)
+
+    except PageNotAnInteger:
+            blogs = paginator.page(1)
+
+
     context = {
         'blogs' : blogs,
-        
+        'paginator' : paginator,
     }
     return render(request, 'blogs/blogs.html', context)
  
