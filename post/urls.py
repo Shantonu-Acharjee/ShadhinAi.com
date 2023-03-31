4f67ebe40f70ede3e7846880045e1c77a4494f4d
Shantonu-Acharjee <shantonuacharjee@gmail.com>
2023-03-31 17:52:24 +0600

    catagory and tag page done

--- post/urls.py
+++ post/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from post.views import blogs, category_blogs
+from post.views import blogs, category_blogs, tag_blogs
 
 
 
 urlpatterns = [
     path('blogs/', blogs, name='blogs'),
     path('category/<str:slug>/', category_blogs, name='category_blogs'),
+    path('tag/<str:slug>/', tag_blogs, name='tag_blogs'),
 ]
--- post/views.py
+++ post/views.py
@@ -35,10 +35,11 @@ def blogs(request):
 def category_blogs(request, slug):
     category = get_object_or_404(Category, slug=slug)
     queryset = category.category_blogs.all()
-    tags = Tag.objects.order_by('-created_date')[:5]
+
+    
     page = request.GET.get('page', 1)
     paginator = Paginator(queryset, 2)
-    all_blogs = Blog.objects.order_by('-created_date')[:5]
+    
     
     try:
         blogs = paginator.page(page)
@@ -50,9 +51,37 @@ def category_blogs(request, slug):
 
     context = {
         "blogs": blogs,
-        "tags": tags,
-        "all_blogs": all_blogs
+        "category_name" : category,
     }
      
     return render(request, 'blogs/category_blogs.html', context)
+
+
+
+
+
+
+def tag_blogs(request, slug):
+    tag = get_object_or_404(Tag, slug=slug)
+    queryset = tag.tag_blogs.all()
+
+    
+    page = request.GET.get('page', 1)
+    paginator = Paginator(queryset, 2)
+    
+    try:
+        blogs = paginator.page(page)
+    except EmptyPage:
+        blogs = paginator.page(1)
+    except PageNotAnInteger:
+        blogs = paginator.page(1)
+        return redirect('blogs')
+
+    context = {
+        "blogs": blogs,
+        "category_name" : tag,
+    }
+     
+    return render(request, 'blogs/category_blogs.html', context)
+ 
  
