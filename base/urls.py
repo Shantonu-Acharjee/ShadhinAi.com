01d4472468f7368c4db540cd5f073774c3401d13
Shantonu-Acharjee <shantonuacharjee@gmail.com>
2023-04-02 10:45:10 +0600

    search function add.

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from base.views import home
+from base.views import home, search_blogs
 
 
 
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('search_blogs/', search_blogs, name='search_blogs'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render, redirect
 from post.models import Blog
-from django.core.paginator import PageNotAnInteger, EmptyPage,Paginator
+from django.core.paginator import PageNotAnInteger, EmptyPage, Paginator
 from . models import Slider
+from django.db.models import Q
 
 
 def home(request):
     queryset = Blog.objects.order_by('-created_date')
     sliders = Slider.objects.order_by('-created_date')
     page = request.GET.get('page', 1)
-    paginator = Paginator(queryset, 2) #enter how many post you want to see on single page
+    # enter how many post you want to see on single page
+    paginator = Paginator(queryset, 2)
 
     try:
         blogs = paginator.page(page)
@@ -17,13 +19,44 @@ def home(request):
         blogs = paginator.page(1)
 
     except PageNotAnInteger:
-            blogs = paginator.page(1)
-            return redirect('home')
+        blogs = paginator.page(1)
+        return redirect('home')
 
     context = {
-        'blogs' : blogs,
-        'paginator' : paginator,
-        'sliders' : sliders,
+        'blogs': blogs,
+        'paginator': paginator,
+        'sliders': sliders,
     }
 
     return render(request, 'home/home.html', context)
+
+
+def search_blogs(request):
+    search_key = request.GET.get('search', None)
+    if search_key:
+        blogs = Blog.objects.filter(
+
+            Q(title__icontains = search_key) |
+            Q(category__title__icontains = search_key) |
+            Q(user__username__icontains = search_key) |
+            Q(tags__title__icontains = search_key)
+
+        ).distinct()
+
+        context ={
+            "blogs" : blogs
+        }
+
+        
+    
+
+    else:
+        blogs = Blog.objects.order_by('-created_date')
+
+        context ={
+            "blogs" : blogs
+        }
+
+
+
+    return render(request, 'base/search.html', context)
